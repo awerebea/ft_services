fe3ab78db836ddb1cc35913d92253a4d51dff610
awerebea <awerebea.21@gmail.com>
2020-09-14 16:36:42 +0300

    phpMyAdmin added, refactored

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+# clean error.log from previoous setup
+rm -f error.log
+
 # сhecking if Docker and Minikube are installed
 prog_missing=0
 for prog in docker minikube
@@ -15,27 +18,28 @@ fi
 
 # start minikube
 minikube start	--vm-driver=virtualbox \
-				--cpus 2 --disk-size=5000mb --memory=2048mb
+				--cpus 2 --memory=2048mb --disk-size=5000mb
 minikube addons enable metallb
-# minikube addons enable metrics-server
-eval $(minikube docker-env) > /dev/null
+minikube addons enable metrics-server
+# minikube addons enable dashboard
+eval $(minikube docker-env) >/dev/null 2>error.log
 
 # load Kubernetes Balancer and data storage volumes
-kubectl apply -f srcs/load_balancer.yaml > /dev/null
-printf "\e[1;34mLoad Balancer\e[0m \e[1;32mconfigured\e[0m\n"
-kubectl apply -f srcs/persistent_volumes.yaml> /dev/null
-printf "\e[1;34mPersistent volumes\e[0m \e[1;32mcreated\e[0m\n"
+kubectl apply -f srcs/load_balancer.yaml >/dev/null 2>error.log
+printf "✔   \e[1;34mLoad Balancer\e[0m \e[1;32mconfigured\e[0m\n"
+kubectl apply -f srcs/persistent_volumes.yaml >/dev/null 2>error.log
+printf "✔   \e[1;34mPersistent volumes\e[0m \e[1;32mcreated\e[0m\n"
 
 # build Docker images and launch them in Kubernetes
 for service in nginx wordpress mysql phpmyadmin
 do
-	docker build -t ${service}_alpine ./srcs/$service > /dev/null
-	kubectl apply -f srcs/$service/$service.yaml > /dev/null
-	printf "\e[1;34m$service\e[0m \e[1;32mstarted\e[0m\n"
+	docker build -t ${service}_alpine ./srcs/$service >/dev/null 2>error.log
+	kubectl apply -f srcs/$service/$service.yaml >/dev/null 2>error.log
+	printf "✔   \e[1;34m$service\e[0m \e[1;32mstarted\e[0m\n"
 done
 
 # info
-printf "\e[1;35mNGINX\e[0m:\t\t\thttp://192.168.99.1\n"
-printf "\e[1;35mWordPress\e[0m (\e[1;32msite\e[0m):\thttp://192.168.99.2:5050\n"
-printf "\e[1;35mWordPress\e[0m (\e[1;31mwp-admin\e[0m):\thttp://192.168.99.2:5050/wp-admin\t(login: \e[1;34madmin\e[0m password: \e[1;34madmin\e[0m)\n"
-printf "\e[1;35mphpMyAdmin:\e[0m:\t\thttp://192.168.99.3:5000\t\t(login: \e[1;34madmin\e[0m password: \e[1;34madmin\e[0m)\n"
+printf "❕  \e[1;35mNGINX\e[0m:\t\t\thttp://192.168.99.10\n"
+printf "❕  \e[1;35mWordPress\e[0m (\e[1;32msite\e[0m):\t\thttp://192.168.99.20:5050\n"
+printf "❕  \e[1;35mWordPress\e[0m (\e[1;31mwp-admin\e[0m):\thttp://192.168.99.20:5050/wp-admin\t(login: \e[1;34madmin\e[0m password: \e[1;34madmin\e[0m)\n"
+printf "❕  \e[1;35mphpMyAdmin:\e[0m:\t\thttp://192.168.99.30:5000\t\t(login: \e[1;34madmin\e[0m password: \e[1;34madmin\e[0m)\n"
--- srcs/mysql/startup.sh
+++ srcs/mysql/startup.sh
@@ -5,5 +5,6 @@ rc default
 rc-service mariadb start
 echo "CREATE DATABASE wordpress;" | mysql
 echo "GRANT ALL ON *.* to 'admin'@'%' IDENTIFIED BY 'admin' WITH GRANT OPTION; FLUSH PRIVILEGES;" | mysql
+mysql wordpress < wordpress.sql
 rc-service mariadb stop
 /usr/bin/mysqld_safe

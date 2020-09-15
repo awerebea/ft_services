17d3c7f12093cb3a4843fe242f6ee638d2bf6a4b
awerebea <awerebea.21@gmail.com>
2020-09-15 21:26:40 +0300

    errors fixed

--- setup.sh
+++ setup.sh
@@ -26,30 +26,31 @@ eval $(minikube docker-env) >/dev/null 2>error.log
 
 # load Kubernetes Balancer and data storage volumes
 kubectl apply -f srcs/load_balancer.yaml >/dev/null 2>error.log
-printf "✔   \e[1;34mLoad Balancer\e[0m \e[1;32mconfigured\e[0m\n"
+kubectl apply -f srcs/persistent_volumes.yaml >/dev/null 2>error.log
 
 # build Docker images and launch them in Kubernetes
 for service in nginx wordpress mysql phpmyadmin influxdb grafana ftps
 do
+	printf "✔   \e[1;34m%-13s\e[0m " "$service"
 	if docker build -t ${service}_alpine ./srcs/$service >/dev/null 2>error.log
 	then
 		if kubectl apply -f srcs/$service/$service.yaml >/dev/null 2>error.log
 		then
-			printf "✔   \e[1;34m$service\e[0m \e[1;32mstarted\e[0m\n"
+			printf "\e[1;32m%s\e[0m\n" "started"
 		else
-			printf "✔   \e[1;34m$service\e[0m \e[1;31merror\e[0m\n"
+			printf "\e[1;31m%s\e[0m\n" "error"
 		fi
 	else
-		printf "✔   \e[1;34m$service\e[0m \e[1;31merror\e[0m\n"
+		printf "\e[1;31m%s\e[0m\n" "error"
 	fi
 done
 
 # info
 printf "\n"
-printf "❕  \e[1;35mNGINX\e[0m (\e[1;32msite\e[0m):\t\thttp://192.168.99.10\n"
-printf "❕  \e[1;35mNGINX\e[0m (\e[1;33mssh\e[0m):\t\tssh \e[1;34muser\e[0m@192.168.99.10\t\t\t(password: \e[1;34muser\e[0m)\n"
-printf "❕  \e[1;35mWordPress\e[0m (\e[1;32msite\e[0m):\t\thttp://192.168.99.20:5050\n"
-printf "❕  \e[1;35mWordPress\e[0m (\e[1;31mwp-admin\e[0m):\thttp://192.168.99.20:5050/wp-admin\t(login: \e[1;34madmin\e[0m password: \e[1;34madmin\e[0m)\n"
-printf "❕  \e[1;35mphpMyAdmin\e[0m:\t\t\thttp://192.168.99.30:5000\t\t(login: \e[1;34madmin\e[0m password: \e[1;34madmin\e[0m)\n"
-printf "❕  \e[1;35mGrafana\e[0m:\t\t\thttp://192.168.99.40:3000\t\t(login: \e[1;34madmin\e[0m password: \e[1;34madmin\e[0m)\n"
-printf "❕  \e[1;35mFTP(S)\e[0m:\t\t\tftp://192.168.99.50:21\t\t\t(login: \e[1;34muser\e[0m  password: \e[1;34muser\e[0m )\n"
+printf "❕  \e[1;35m%-10s\e[0m \e[1;32m%-8s\e[0m   %-34s  %s \e[1;34m%-5s\e[0m %s \e[1;34m%-5s\e[0m%s\n" "nginx" "site" "http://192.168.99.10" "" "" "" "" ""
+printf "❕  \e[1;35m%-10s\e[0m \e[1;33m%-8s\e[0m   %-34s  %s \e[1;34m%-5s\e[0m %s \e[1;34m%-5s\e[0m%s\n" "nginx" "ssh" "ssh user@192.168.99.10" "(login:" "user" "password:" "user" ")"
+printf "❕  \e[1;35m%-10s\e[0m \e[1;32m%-8s\e[0m   %-34s  %s \e[1;34m%-5s\e[0m %s \e[1;34m%-5s\e[0m%s\n" "wordpress" "site" "http://192.168.99.20:5050" "" "" "" ""
+printf "❕  \e[1;35m%-10s\e[0m \e[1;31m%-8s\e[0m   %-34s  %s \e[1;34m%-5s\e[0m %s \e[1;34m%-5s\e[0m%s\n" "wordpress" "wp-admin" "http://192.168.99.20:5050/wp-admin" "(login:" "admin" "password:" "admin" ")"
+printf "❕  \e[1;35m%-10s\e[0m \e[1;32m%-8s\e[0m   %-34s  %s \e[1;34m%-5s\e[0m %s \e[1;34m%-5s\e[0m%s\n" "phpmyadmin" "" "http://192.168.99.30:5000" "(login:" "admin" "password:" "admin" ")"
+printf "❕  \e[1;35m%-10s\e[0m \e[1;32m%-8s\e[0m   %-34s  %s \e[1;34m%-5s\e[0m %s \e[1;34m%-5s\e[0m%s\n" "grafana" "" "http://192.168.99.40:3000" "(login:" "admin" "password:" "admin" ")"
+printf "❕  \e[1;35m%-10s\e[0m \e[1;32m%-8s\e[0m   %-34s  %s \e[1;34m%-5s\e[0m %s \e[1;34m%-5s\e[0m%s\n" "ftps" "" "ftp://192.168.99.50:21" "(login:" "user" "password:" "user" ")"
